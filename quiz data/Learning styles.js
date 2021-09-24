509c3119e167dcc8ee2d2e2dbea3f8270dbdaccc
AndrewWilkinson <AndrewWilkinson@users.noreply.github.com>
2021-09-24 12:33:02 +0100

    added feedback transition and seperated questions file

---
+++ quiz data/Learning styles.js
@@ -0,0 +1,120 @@
+let yourQuiz = [["What's Your Learning Style", { instructions: "For these questions, choose the first answer that comes to mind." }],
+["Three Different Learning Styles",
+    {
+        a: ["Visual Learner:", "<ul><li> take numerous detailed notes</li><li> tend to sit in the front</li><li> are usually neat and clean</li><li> often close their eyes to visualize or remember something</li><li> find something to watch if they are bored</li><li> like to see what they are learning</li><li> benefit from illustrations and presentations that use color</li><li> are attracted to written or spoken language rich in imagery</li><li> prefer stimuli to be isolated from auditory and kinesthetic distraction</li><li> find passive surroundings ideal</li><ul>"],
+        b: ["Auditory Learner:", "<ul><li> sit where they can hear but needn't pay attention to what is happening in front</li><li> may not coordinate colors or clothes, but can explain why they are wearing what they are wearing and why</li><li> hum or talk to themselves or others when bored</li><li> acquire knowledge by reading aloud</li><li> remember by verbalizing lessons to themselves (if they don't they have difficulty reading maps or diagrams or handling conceptual assignments like mathematics).</li></ul>"],
+        c: ["Kinesthetic Learner:", "<ul><li> need to be active and take frequent breaks</li><li> speak with their hands and with gestures</li><li> remember what was done, but have difficulty recalling what was said or seen</li><li> find reasons to tinker or move when bored</li><li> rely on what they can directly experience or perform</li><li> activities such as cooking, construction, engineering and art help them perceive and learn</li><li> enjoy field trips and tasks that involve manipulating materials</li><li> sit near the door or someplace else where they can easily get up and move around</li><li> are uncomfortable in classrooms where they lack opportunities for hands-on experience</li><li> communicate by touching and appreciate physically expressed encouragement, such as a pat on the back</li></ul>"]
+    }
+],
+["When you study for a test, would you rather",
+    {
+        a: "read notes, read headings in a book, and look at diagrams and illustrations.",
+        b: "have someone ask you questions, or repeat facts silently to yourself.",
+        c: "write things out on index cards and make models or diagrams."
+    }
+],
+["Which of these do you do when you listen to music?",
+    {
+        a: "daydream (see things that go with the music)",
+        b: "hum along",
+        c: "move with the music, tap your foot, etc."
+    },
+],
+["When you work at solving a problem do you",
+    {
+        a: "make a list, organize the steps, and check them off as they are done",
+        b: "make a few phone calls and talk to friends or experts",
+        c: "make a model of the problem or walk through all the steps in your mind"
+    }
+],
+["When you read for fun, do you prefer",
+    {
+        a: "a travel book with a lot of pictures in it",
+        b: "a mystery book with a lot of conversation in it",
+        c: "a book where you answer questions and solve problems"
+    }
+],
+["To learn how a computer works, would you rather",
+    {
+        a: "watch a movie about it",
+        b: "listen to someone explain it",
+        c: "take the computer apart and try to figure it out for yourself"
+    }
+],
+["You have just entered a science museum, what will you do first?",
+    {
+        a: "look around and find a map showing the locations of the various exhibits",
+        b: "talk to a museum guide and ask about exhibits",
+        c: "go into the first exhibit that looks interesting, and read directions later"
+    }
+],
+["What kind of restaurant would you rather not go to?",
+    {
+        a: "one with the lights too bright",
+        b: "one with the music too loud",
+        c: "one with uncomfortable chairs"
+    }
+],
+["Would you rather go to",
+    {
+        a: "an art class",
+        b: "a music class",
+        c: "an exercise class"
+    }
+],
+["Which are you most likely to do when you are happy?",
+    {
+        a: "grin",
+        b: "shout with joy",
+        c: "jump for joy"
+    }
+],
+["If you were at a party, what would you be most likely to remember the next day?",
+    {
+        a: "the faces of the people there, but not the names",
+        b: "the names but not the faces",
+        c: "the things you did and said while you were there"
+    }
+],
+["When you see the word \"d - o - g\", what do you do first?",
+    {
+        a: "think of a picture of a particular dog",
+        b: "say the word \"dog\" to yourself silently",
+        c: "sense the feeling of being with a dog (petting it, running with it, etc.)"
+    }
+],
+["When you tell a story, would you rather",
+    {
+        a: "write it",
+        b: "tell it out loud",
+        c: "act it out"
+    }
+],
+["What is most distracting for you when you are trying to concentrate?",
+    {
+        a: "visual distractions",
+        b: "noises",
+        c: "other sensations like, hunger, tight shoes, or worry"
+    }
+],
+["What are you most likely to do when you are angry?",
+    {
+        a: "scowl",
+        b: "shout or \"blow up\"",
+        c: "stomp off and slam doors"
+    }
+],
+["When you aren't sure how to spell a word, which of these are you most likely to do?",
+    {
+        a: "write it out to see if it looks right",
+        b: "sound it out",
+        c: "write it out to see if it feels right"
+    }
+],
+["Which are you most likely to do when standing in a long line at the movies?",
+    {
+        a: "look at posters advertising other movies",
+        b: "talk to the person next to you",
+        c: "tap your foot or move around in some other way"
+    }
+]];
---
+++ quiz data/another quiz.js
@@ -0,0 +1,16 @@
+let yourQuiz = [["Learn about our instructors", { instructions: "The choice is yours" }],
+["Our instructors",
+    {
+        a: ["Colin:", "<ul><li>Uses Windows</li><ul>"],
+        b: ["Neil:", "<ul><li>Uses macOS</li></ul>"],
+        c: ["Vincent:", "<ul><li>Is American</li></ul>"]
+    }
+],
+["Which instructor do you need to know more about?",
+    {
+        a: "Colin",
+        b: "Neil",
+        c: "Vincent"
+    }
+],
+];
